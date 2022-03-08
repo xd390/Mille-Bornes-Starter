c2942b32f0d7f03d0f8420473dc1550743ff1810
xd390 <ngpananthony@gmail.com>
2022-03-08 17:58:20 +1100

    Ajout de la class joueur/Carte Todo faire la class plateau

---
+++ src/main/java/nc/unc/gl/borne/Carte.java
@@ -0,0 +1,18 @@
+package nc.unc.gl.borne;
+
+public class Carte{
+    enum Type{
+        attaque,
+        parade,
+        botte,
+        borne,
+
+    }
+
+    protected int id;
+    protected String nom;
+    protected Type type;
+
+
+
+}
--- src/main/java/nc/unc/gl/borne/MainView.java
+++ src/main/java/nc/unc/gl/borne/MainView.java
@@ -2,13 +2,21 @@ package nc.unc.gl.borne;
 
 import com.vaadin.flow.component.HtmlContainer;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.router.Route;
 
 @Route
 @Tag("borne-main")
 public class MainView extends HtmlContainer {
 
-    public MainView() {
-        this.add("Hello World");
+    public MainView()
+    {
+        addClassName("login-rich-content");
+
+        LoginForm loginForm = new LoginForm();
+        loginForm.getElement().getThemeList().add("dark");
+        add(loginForm);
+        // Prevent the example from stealing focus when browsing the documentation
+        loginForm.getElement().setAttribute("no-autofocus", "");
     }
 }
