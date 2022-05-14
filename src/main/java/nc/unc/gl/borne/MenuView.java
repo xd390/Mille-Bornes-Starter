3a0f857cfe701b05946916ee6d218e5e8af0973e
anthony ng pan <ngpananthony@gmail.com>
2022-05-14 15:57:26 +1100

    remise au norme de plateau

--- src/main/java/nc/unc/gl/borne/MenuView.java
+++ src/main/java/nc/unc/gl/borne/MenuView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -14,7 +15,7 @@ import com.vaadin.flow.router.Route;
 @Route("menu")
 @PageTitle("Menu")
 public class MenuView extends VerticalLayout {
-
+    private final TextField pseudo;
 
     public MenuView(){
         addClassName("-view");
@@ -23,17 +24,25 @@ public class MenuView extends VerticalLayout {
         setJustifyContentMode(JustifyContentMode.CENTER);
 
 
-
         Div menu = new Div();
-        TextField pseudo = new TextField();
+        pseudo = new TextField();
         pseudo.setRequired(true);
         pseudo.setLabel("Entrer votre pseudo");
         Button play = new Button();
+        play.addClickListener(buttonClickEvent -> jouer());
         play.setText("Jouer");
         menu.add(pseudo,play);
-        Image titre = new Image("cartes/back.PNG","Titre");
+        Image titre = new Image("Images/MilleBorne.PNG","Titre");
         add(titre);
         add(menu);
+    }
+    private void jouer(){
+        if(pseudo.getOptionalValue().isPresent()){
+
+        }
+        else{
+            Notification.show("Vous devez entrer un pseudo");
+        }
     }
 
 }
